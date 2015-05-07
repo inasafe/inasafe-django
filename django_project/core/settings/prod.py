5553dc46fbdd116c0f4b42fc0a4e697264e2d3d8
Akbar Gumbira <akbargumbira@gmail.com>
2015-05-07 16:28:26 +0700

    Move pipeline setting related to prod setting.

--- django_project/core/settings/prod.py
+++ django_project/core/settings/prod.py
@@ -17,6 +17,14 @@ DATABASES = {
     }
 }
 
+# define template function (example for underscore)
+# PIPELINE_TEMPLATE_FUNC = '_.template'
+PIPELINE_YUI_BINARY = '/usr/bin/yui-compressor'
+PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.yui.YUICompressor'
+PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.yui.YUICompressor'
+PIPELINE_YUI_JS_ARGUMENTS = '--nomunge'
+PIPELINE_DISABLE_WRAPPER = True
+
 # Comment if you are not running behind proxy
 USE_X_FORWARDED_HOST = True
 
