84fac71c5fa778c32de00f3a87df5687f82fbe65
Jannes123 <jannes@kartoza.com>
2015-06-22 17:45:14 +0200

    populate test view

--- django_project/realtime/urls.py
+++ django_project/realtime/urls.py
@@ -1,19 +1,37 @@
 # coding=utf-8
 from django.conf.urls import url
-from realtime.views import earthquake
+from realtime.views.earthquake import (
+    earthquake_list,
+    earthquake_detail,
+    index,
+    get_earthquakes,
+    populate
+    )
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import patterns, url
 
 urlpatterns = [
     url(r'^api/v1/earthquake/$',
-        earthquake.earthquake_list,
+        earthquake_list,
         name='earthquake_list'),
     url(r'^api/v1/earthquake/(?P<shake_id>[-\w]+)/$',
-        earthquake.earthquake_detail,
-        name='earthquake_detail'),
-    url(r'^$', 'realtime.views.index', name='index'),
-    url(r'^get_earthquakes/.*$', 'realtime.views.get_earthquakes', name='get_earthquakes'),
+        earthquake_detail,
+        name='earthquake_detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
+urlpatterns.extend(
+    patterns(
+        '',
+        url(r'^$',
+            index,
+            name='index'),
+        url(r'^get_earthquakes/.*$',
+            get_earthquakes,
+            name='get_earthquakes'),
+        url(r'^populate$',
+            populate,
+            name='populate'),
+    )
+)
--- django_project/realtime/views/earthquake.py
+++ django_project/realtime/views/earthquake.py
@@ -1,15 +1,78 @@
 # coding=utf-8
 from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext, loader
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 from realtime.models.earthquake import Earthquake
 from realtime.serializers.earthquake_serializer import EarthquakeSerializer
+from realtime.app_settings import LEAFLET_TILES
+from realtime.forms import FilterForm
+from realtime.tests.model_factories import EarthquakeFactory
 
 __author__ = 'Rizky Maulana Nugraha "lucernae" <lana.pcfre@gmail.com>'
 __date__ = '19/06/15'
 
+
+def index(request):
+    """Index page of user map.
+
+    :param request: A django request object.
+    :type request: request
+
+    :returns: Response will be a leaflet map page.
+    :rtype: HttpResponse
+    """
+
+    if request.method == 'POST':
+        pass
+    else:
+        form = FilterForm()
+
+    leaflet_tiles = dict(
+        url=LEAFLET_TILES[1],
+        attribution=LEAFLET_TILES[2]
+        )
+
+    context = RequestContext(request)
+    context['leaflet_tiles'] = leaflet_tiles
+    return render_to_response(
+            'realtime/index.html',
+            {'form': form},
+            context_instance=context
+            )
+
+
+def get_earthquakes(request):
+    """Return a json document of earthquakes of the project.
+    :param request: A django request object.
+    :type request: request
+    """
+    if request.method == 'GET':
+
+        # Get data:
+        events = Earthquake.objects.all()
+        context = {'events': events}
+        events_json = loader.render_to_string(
+            'realtime/events.json',
+            context_instance=RequestContext(request, context))
+        # Return Response
+        return HttpResponse(events_json, content_type='application/json')
+    else:
+        raise Http404
+
+
+def populate(request):
+    """saves an earthquake to the db.(just for testing)
+    :param request: A django request object.
+    :type request: request
+    """
+    if request.method == 'GET':
+        earthquake = EarthquakeFactory.create()
+        earthquake.save()
+        return index(request)
 
 @api_view(['GET', 'POST'])
 def earthquake_list(request, format=None):
