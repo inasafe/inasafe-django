cfd80c6c6dd2ba1dd713129f839e6a564ecba1cb
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-10-14 10:19:20 +0700

    Finalizing sftp backup

--- deployment/docker-backup/backups.sh
+++ deployment/docker-backup/backups.sh
@@ -9,6 +9,7 @@ MYDATE=`date +%d-%B-%Y`
 MONTH=$(date +%B)
 YEAR=$(date +%Y)
 MYBASEDIR=/backups
+TARGETDIR=/pg_backup
 MYBACKUPDIR=${MYBASEDIR}/${YEAR}/${MONTH}
 mkdir -p ${MYBACKUPDIR}
 cd ${MYBACKUPDIR}
@@ -26,6 +27,9 @@ do
   echo "Backing up $DB"  >> /var/log/cron.log
   FILENAME=${MYBACKUPDIR}/${DUMPPREFIX}_${DB}.${MYDATE}.dmp
   pg_dump -i -Fc -f ${FILENAME} -x -O ${DB}
-  echo "push backup to remote server" >> /var/log/cron.log
-  /usr/bin/python /sftp_remote.py ${FILENAME} >> /var/log/cron.log
+
+  # create link for latest dump
+  echo "Create link for latest dump"
+  ln -sf $FILENAME $MYBASEDIR/latest.dmp
+  cp $MYBASEDIR/latest.dmp $TARGETDIR/latest.dmp
 done
--- deployment/docker-backups/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# source this before running the script
-source /pgenv.sh
-
-/usr/bin/python /cleanup.py
