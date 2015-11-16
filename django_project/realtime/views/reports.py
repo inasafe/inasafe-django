42e780ce3b45994014c883751cb73c22c1e15899
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-11-17 02:24:48 +0700

    develop: add latest report url

--- django_project/realtime/views/reports.py
+++ django_project/realtime/views/reports.py
@@ -9,6 +9,15 @@ __date__ = '19/06/15'
 
 
 def report_pdf(request, shake_id, language=u'id', language2=u'id'):
+    """Return PDF file of desired shake id and language
+
+    :param request: Django request object
+    :param shake_id: Shake ID
+    :param language: Language of the report
+    :param language2: Language of the report, only a dummy variable, because
+        of URL pattern
+    :return: PDF file
+    """
     try:
         report = EarthquakeReport.objects.get(
             earthquake__shake_id=shake_id,
@@ -33,6 +42,15 @@ def report_pdf(request, shake_id, language=u'id', language2=u'id'):
 
 
 def report_image(request, shake_id, language=u'id', language2=u'id'):
+    """Return PNG file of desired shake id and language
+
+    :param request: Django request object
+    :param shake_id: Shake ID
+    :param language: Language of the report
+    :param language2: Language of the report, only a dummy variable, because
+        of URL pattern
+    :return: PNG file
+    """
     try:
         report = EarthquakeReport.objects.get(
             earthquake__shake_id=shake_id,
@@ -57,6 +75,17 @@ def report_image(request, shake_id, language=u'id', language2=u'id'):
 
 
 def report_thumbnail(request, shake_id, language=u'id', language2=u'id'):
+    """Return Thumbnail file of desired shake id and language
+
+    Thumbnail file is a smaller-downsized version of Image report
+
+    :param request: Django request object
+    :param shake_id: Shake ID
+    :param language: Language of the report
+    :param language2: Language of the report, only a dummy variable, because
+        of URL pattern
+    :return: Thumbnail file
+    """
     try:
         report = EarthquakeReport.objects.get(
             earthquake__shake_id=shake_id,
@@ -78,3 +107,36 @@ def report_thumbnail(request, shake_id, language=u'id', language2=u'id'):
                 return HttpResponse(f, content_type='image/png')
         except IOError:
             raise Http404()
+
+
+def latest_report(request, report_type=u'pdf', language=u'id'):
+    """Return the latest report file of desired report type and language
+
+    :param request: Django request object
+    :param report_type: Report type, can be 'pdf', 'png', or 'thumbnail'
+    :param language: Language of the report
+    :return: Desired report file
+    """
+    try:
+        report = EarthquakeReport.objects.filter(
+            language=language).order_by('earthquake__shake_id').last()
+
+        if not report:
+            raise Http404()
+
+        if report_type == 'pdf':
+            the_file = report.report_pdf.read()
+            content_type = 'application/pdf'
+        elif report_type == 'png':
+            the_file = report.report_image.read()
+            content_type = 'image/png'
+        elif report_type == 'thumbnail':
+            the_file = report.report_thumbnail.read()
+            content_type = 'image/png'
+
+        if not the_file:
+            raise Http404()
+
+        return HttpResponse(the_file, content_type=content_type)
+    except (EarthquakeReport.DoesNotExist, IOError, AttributeError):
+        raise Http404()
