78bd821308b89ca3a996db17d620f9b858bb03a6
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-12-03 05:20:07 +0700

    develop #120: add Flood REST Tests

--- django_project/realtime/tests/test_api_flood.py
+++ django_project/realtime/tests/test_api_flood.py
@@ -1,7 +1,14 @@
 # coding=utf-8
 import datetime
 import filecmp
+import logging
 import os
+
+import requests
+import shutil
+from django.core.files.temp import NamedTemporaryFile
+import urlparse
+from requests.status_codes import codes
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -22,8 +29,10 @@ from realtime.serializers.pagination_serializer import \
 from realtime.tests.utilities import download_url
 
 __author__ = 'Rizky Maulana Nugraha <lana.pcfre@gmail.com>'
-
 __date__ = '11/30/15'
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class TestFlood(APITestCase):
@@ -32,6 +41,66 @@ class TestFlood(APITestCase):
 
     def data_path(self, filename):
         return u'realtime/tests/data/flood/'+filename
+
+    def assertEqualDictionaryWithFiles(self, dict_value, dict_control):
+        """Compare dict control with dict value
+
+        :param dict_value: The actual value to compare to a certain control
+            variable
+        :type dict_value: dict
+
+        :param dict_control: The control value as defined as expected value
+            that dict_value should be. The file in the dict_control should be
+            from self.data_path directory
+        :type dict_control: dict
+        """
+        for key, value in dict_control.iteritems():
+            if isinstance(value, File):
+                # tries to do a file comparison
+                control_path = os.path.abspath(value.name)
+                if isinstance(dict_value[key], File):
+                    # File by file comparison
+                    value_path = os.path.abspath(
+                        os.path.join(
+                            settings.MEDIA_ROOT,
+                            dict_value[key].name
+                        ))
+                    message = 'File %s != %s' % (
+                        value_path,
+                        control_path)
+                    self.assertTrue(
+                        filecmp.cmp(value_path, control_path, shallow=False),
+                        message)
+                elif isinstance(dict_value[key], str):
+                    # the value is a url
+                    # download the file first
+                    with download_url(self.client, dict_value[key]) as f:
+                        message = 'File %s != %s' % (
+                            f.name,
+                            control_path)
+                        file_equals = filecmp.cmp(
+                                f.name, control_path, shallow=False)
+                        if not file_equals:
+                            # There can be a reason why the file is not
+                            # equals. For example, when the file is
+                            # compressed on delivery. So, do a little bit
+                            # heuristic.
+                            value_size = os.stat(f.name).st_size
+                            control_size = os.stat(control_path).st_size
+                            # consider it equals if the difference in size
+                            # is less than 1% fraction
+                            difference = abs(control_size - value_size)
+                            file_equals = (
+                                difference < control_size * 0.20)
+                            warning_message = (
+                                'Consider it equal because the fraction '
+                                'difference - difference is %f %% - %d bytes: %s' % (
+                                    difference * 100.0 / control_size,
+                                    difference,
+                                    file_equals
+                                ))
+                            LOGGER.warning(warning_message)
+                        self.assertTrue(file_equals, message)
 
     def setUp(self):
         if settings.TESTING:
@@ -48,8 +117,19 @@ class TestFlood(APITestCase):
             source=u'Peta Jakarta',
             region=u'Jakarta',
             impact_layer=File(open(
-                self.data_path('impact_layer.zip')
+                self.data_path('impact.zip')
             ))
+        )
+
+        flood_report_en = FloodReport.objects.create(
+            flood=flood,
+            language='en',
+            impact_report=File(open(
+                self.data_path('impact-table-en.pdf')
+            )),
+            impact_map=File(open(
+                self.data_path('impact-map-en.pdf')
+            )),
         )
 
         # create test user
@@ -74,7 +154,7 @@ class TestFlood(APITestCase):
             'source': u'Peta Jakarta',
             'region': u'Jakarta',
             'impact_layer': File(open(
-                self.data_path('impact_layer.zip')
+                self.data_path('impact.zip')
             ))
         }
 
@@ -86,37 +166,20 @@ class TestFlood(APITestCase):
         # Test that serializer saves dict to model
         self.assertTrue(flood)
         serializer = FloodSerializer(flood)
-        for key, value in flood_dict.iteritems():
-            if isinstance(value, File):
-                media_relative_path = os.path.relpath(
-                    serializer.data[key], '/media')
-                media_path = os.path.join(
-                    settings.MEDIA_ROOT,
-                    media_relative_path
-                )
-                # Test that file saved is the same in the media
-                self.assertTrue(
-                    filecmp.cmp(
-                        media_path,
-                        flood_dict['impact_layer'].name
-                    )
-                )
-            else:
-                # that model value equals dict value
-                self.assertEqual(serializer.data[key], value)
+        self.assertEqualDictionaryWithFiles(serializer.data, flood_dict)
         flood.delete()
 
     def test_flood_report_serializer(self):
         """Test for Report Serializer"""
         report_dict = {
             'event_id': u'2015112518-3-rw',
-            'language': u'en',
-            # 'impact_report': File(
-            #     open()
-            # ),
-            # 'impact_map': File(
-            #     open()
-            # )
+            'language': u'id',
+            'impact_report': File(
+                open(self.data_path('impact-table-id.pdf'))
+            ),
+            'impact_map': File(
+                open(self.data_path('impact-map-id.pdf'))
+            )
         }
         serializer = FloodReportSerializer(data=report_dict)
         # Test that Serializer from dict to model works
@@ -128,7 +191,7 @@ class TestFlood(APITestCase):
         try:
             report = FloodReport.objects.get(
                 flood__event_id=u'2015112518-3-rw',
-                language=u'en'
+                language=u'id'
             )
         except FloodReport.DoesNotExist:
             pass
@@ -142,8 +205,7 @@ class TestFlood(APITestCase):
 
         serializer = FloodReportSerializer(report)
         # Test member of the serialized model equals dict value
-        self.assertEqual(serializer.data['event_id'], u'2015112518-3-rw')
-        self.assertEqual(serializer.data['language'], u'en')
+        self.assertEqualDictionaryWithFiles(serializer.data, report_dict)
 
     def test_flood_list(self):
         response = self.client.get(reverse('realtime:flood_list'))
@@ -172,8 +234,7 @@ class TestFlood(APITestCase):
         }
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         actual_flood = response.data
-        for key, value in expected_flood.iteritems():
-            self.assertEqual(actual_flood[key], value)
+        self.assertEqualDictionaryWithFiles(actual_flood, expected_flood)
 
     def test_flood_post(self):
         flood_json = {
@@ -183,7 +244,7 @@ class TestFlood(APITestCase):
             'source': u'Peta Jakarta',
             'region': u'Jakarta',
             'impact_layer': File(
-                open(self.data_path('impact_layer.zip'))
+                open(self.data_path('impact.zip'))
             )
         }
 
@@ -202,13 +263,7 @@ class TestFlood(APITestCase):
         self.assertEqual(serializer.count, 2)
         flood = Flood.objects.get(event_id=flood_json['event_id'])
         serializer = FloodSerializer(flood)
-        for key, value in flood_json.iteritems():
-            if isinstance(value, File):
-                url = serializer.data[key]
-                with download_url(self.client, url) as f:
-                    filecmp.cmp(f.name, value.name)
-            else:
-                self.assertEqual(value, serializer.data[key])
+        self.assertEqualDictionaryWithFiles(serializer.data, flood_json)
 
         flood.delete()
 
@@ -231,7 +286,7 @@ class TestFlood(APITestCase):
 
         # put file data using multipart
         flood_json['impact_layer'] = File(
-            open(self.data_path('impact_layer.zip')))
+            open(self.data_path('impact.zip')))
 
         response = self.client.put(
             reverse(
@@ -262,7 +317,7 @@ class TestFlood(APITestCase):
                     settings.MEDIA_ROOT,
                     flood.impact_layer.name
                 ),
-                self.data_path('impact_layer.zip')
+                self.data_path('impact.zip')
             )
         )
         flood.delete()
@@ -275,7 +330,7 @@ class TestFlood(APITestCase):
             'source': u'Peta Jakarta',
             'region': u'Jakarta',
             'impact_layer': File(
-                open(self.data_path('impact_layer.zip'))
+                open(self.data_path('impact.zip'))
             )
         }
 
@@ -326,3 +381,246 @@ class TestFlood(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 1)
 
+    def test_flood_report_post(self):
+        report_dict = {
+            'event_id': u'2015112518-3-rw',
+            'language': u'id',
+            'impact_report': File(
+                open(self.data_path('impact-table-id.pdf'))
+            ),
+            'impact_map': File(
+                open(self.data_path('impact-map-id.pdf'))
+            )
+        }
+
+        response = self.client.post(
+            reverse(
+                'realtime:flood_report_list',
+                kwargs={'event_id': report_dict['event_id']}
+            ),
+            report_dict,
+            format='multipart'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.get(reverse('realtime:flood_report_list'))
+
+        # Test the json returns
+        self.assertEqual(response.data['count'], 2)
+        self.assertEqualDictionaryWithFiles(
+            response.data['results'][0], report_dict)
+
+        # Test using serializer
+        serializer = PageNumberPaginationSerializer(
+            serializer_class=FloodReportSerializer,
+            data=response.data)
+        self.assertEqual(serializer.count, 2)
+        self.assertEqualDictionaryWithFiles(serializer.results[0], report_dict)
+        report = FloodReport.objects.get(
+            flood__event_id=report_dict['event_id'],
+            language=u'id')
+        serializer = FloodReportSerializer(report)
+        self.assertEqualDictionaryWithFiles(serializer.data, report_dict)
+
+        report.delete()
+
+    def test_flood_report_detail(self):
+        report_dict = {
+            'event_id': u'2015112518-3-rw',
+            'language': u'en',
+            'impact_report': File(
+                open(self.data_path('impact-table-en.pdf'))
+            ),
+            'impact_map': File(
+                open(self.data_path('impact-map-en.pdf'))
+            )
+        }
+
+        response = self.client.get(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqualDictionaryWithFiles(response.data, report_dict)
+
+    def test_flood_report_put(self):
+        report_dict = {
+            'event_id': u'2015112518-3-rw',
+            'language': u'id'
+        }
+
+        response = self.client.post(
+            reverse(
+                'realtime:flood_report_list',
+                kwargs={'event_id': report_dict['event_id']}
+            ),
+            report_dict,
+            format='multipart'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.get(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that impact map and report is empty
+        self.assertFalse(response.data.get('impact_map'))
+        self.assertFalse(response.data.get('impact_report'))
+
+        # Update data using put
+        report_dict['impact_report'] = File(
+                open(self.data_path('impact-table-id.pdf'))
+            )
+        report_dict['impact_map'] = File(
+                open(self.data_path('impact-map-id.pdf'))
+            )
+
+        response = self.client.put(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ),
+            report_dict,
+            format='multipart'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that impact map and report is NOT empty
+        self.assertTrue(response.data.get('impact_map'))
+        self.assertTrue(response.data.get('impact_report'))
+        # and correct
+        self.assertEqualDictionaryWithFiles(response.data, report_dict)
+
+        # Now change some values
+        report_dict['impact_map'] = File(
+                open(self.data_path('impact-map-en.pdf'))
+            )
+        report_dict.pop('impact_report')
+
+        response = self.client.put(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ),
+            report_dict,
+            format='multipart'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check the change is reflected and correct
+        self.assertEqualDictionaryWithFiles(response.data, report_dict)
+
+        report = FloodReport.objects.get(
+            flood__event_id=report_dict['event_id'],
+            language=report_dict['language'])
+        report.delete()
+
+    def test_flood_report_delete(self):
+        report_dict = {
+            'event_id': u'2015112518-3-rw',
+            'language': u'id',
+            'impact_report': File(
+                open(self.data_path('impact-table-id.pdf'))
+            ),
+            'impact_map': File(
+                open(self.data_path('impact-map-id.pdf'))
+            )
+        }
+
+        response = self.client.post(
+            reverse(
+                'realtime:flood_report_list',
+                kwargs={'event_id': report_dict['event_id']}
+            ),
+            report_dict,
+            format='multipart'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.get(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # check that post succes
+        report = FloodReport.objects.get(
+            flood__event_id=report_dict['event_id'],
+            language=report_dict['language']
+        )
+        self.assertTrue(report)
+
+        serializer = FloodReportSerializer(report)
+
+        # Check content the same
+        self.assertEqualDictionaryWithFiles(serializer.data, report_dict)
+
+        # delete using REST
+
+        response = self.client.delete(
+            reverse(
+                'realtime:flood_report_detail',
+                kwargs={
+                    'event_id': report_dict['event_id'],
+                    'language': report_dict['language']
+                }
+            ))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # Check report doesn't exists anymore
+        with self.assertRaises(FloodReport.DoesNotExist):
+            FloodReport.objects.get(
+                flood__event_id=report_dict['event_id'],
+                language=report_dict['language']
+            )
