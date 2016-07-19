0db542c2eb814c308dd48446fb9486006a4eb597
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2016-07-19 12:16:04 +0700

    merge migrations

--- django_project/realtime/migrations/0015_ash.py
+++ django_project/realtime/migrations/0015_ash.py
@@ -28,4 +28,14 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
+        migrations.AlterModelOptions(
+            name='floodeventboundary',
+            options={'verbose_name_plural': 'Flood Event Boundaries'},
+        ),
+        migrations.AddField(
+            model_name='earthquake',
+            name='felt',
+            field=models.BooleanField(default=False, help_text=b"Set to True if this particular event showed up as felt Earthquake in BMKG's List", verbose_name=b'Felt Earthquake'),
+            preserve_default=True,
+        ),
     ]
