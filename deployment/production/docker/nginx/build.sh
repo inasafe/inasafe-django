9ba4cc2e20f9200a3a54396b0593fac7b52e8756
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-01-11 16:23:17 +0700

    Update docker build scripts for production

--- deployment/production/docker/nginx/build.sh
+++ deployment/production/docker/nginx/build.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-REPO_NAME=kartoza
-IMAGE_NAME=inasafe-django_nginx
+
+if [ -z $REPO_NAME ]; then
+	REPO_NAME=kartoza
+fi
+
+if [ -z $IMAGE_NAME ]; then
+	IMAGE_NAME=inasafe-django_nginx
+fi
+
 if [ -z $TAG_NAME ]; then
 	TAG_NAME=latest
-fiop
+fi
+
+if [ -z $BUILD_ARGS ]; then
+	BUILD_ARGS="--pull --no-cache"
 fi
 
 echo "Build: $REPO_NAME/$IMAGE_NAME:$TAG_NAME"
+echo "Build Args: $BUILD_ARGS"
 
-docker build -t ${REPO_NAME}/${IMAGE_NAME} .
+docker build -t ${REPO_NAME}/${IMAGE_NAME} ${BUILD_ARGS} .
 docker tag ${REPO_NAME}/${IMAGE_NAME}:latest ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
 docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
