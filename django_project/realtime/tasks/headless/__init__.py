952093ec34fc193fd6a140ad7c20929e948da3d8
Ismail Sunni <imajimatika@gmail.com>
2018-02-05 12:38:42 +0700

    Add tasks for InaSAFE headless.

---
+++ django_project/realtime/tasks/headless/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+"""Headless tasks proxy."""
+
+__copyright__ = "Copyright 2018, The InaSAFE Project"
+__license__ = "GPL version 3"
+__email__ = "info@inasafe.org"
+__revision__ = '$Format:%H$'
