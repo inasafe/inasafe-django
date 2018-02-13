a6b9496cb730908ba3b13054277d1049b019e917
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-02-13 11:51:20 +0700

    Fix Ash landing page for Django 1.8 changes
    - exclude django-devserver
    - fix wrong signals hook for earthquake
    - draft for ash hazard hook

--- django_project/realtime/signals/earthquake.py
+++ django_project/realtime/signals/earthquake.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
 from realtime.app_settings import LOGGER_NAME
-from realtime.models.ash import Ash
+from realtime.models import Earthquake
 from realtime.tasks.earthquake import generate_event_report
 
 __author__ = 'Rizky Maulana Nugraha <lana.pcfre@gmail.com>'
@@ -18,7 +18,7 @@ LOGGER = logging.getLogger(LOGGER_NAME)
 LOGGER.info('Earthquake Signals registered')
 
 
-@receiver(post_save, sender=Ash)
+@receiver(post_save, sender=Earthquake)
 def earthquake_post_save(sender, instance, **kwargs):
     """Extract impact layer of the flood"""
     try:
