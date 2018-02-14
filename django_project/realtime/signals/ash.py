68414f8384f0cc98036ada3abde158bb0d189a5d
Ismail Sunni <imajimatika@gmail.com>
2018-02-14 17:49:52 +0700

    Make reporting works.

--- django_project/realtime/signals/ash.py
+++ django_project/realtime/signals/ash.py
@@ -6,7 +6,8 @@ from django.dispatch.dispatcher import receiver
 
 from realtime.app_settings import LOGGER_NAME
 from realtime.models.ash import Ash
-from realtime.tasks.ash import generate_event_report, run_ash_analysis
+from realtime.tasks.ash import (
+    generate_event_report, run_ash_analysis, generate_ash_report)
 
 __author__ = 'Rizky Maulana Nugraha <lana.pcfre@gmail.com>'
 __date__ = '7/18/16'
@@ -18,6 +19,7 @@ LOGGER = logging.getLogger(LOGGER_NAME)
 LOGGER.info('Ash Signals registered')
 
 
+# TODO: These ifs are ugly, need to refactor it.
 @receiver(post_save, sender=Ash)
 def ash_post_save(sender, **kwargs):
     """Extract impact layer of the flood"""
@@ -31,9 +33,24 @@ def ash_post_save(sender, **kwargs):
                     # It's either pending or success, do nothing
                     if (instance.analysis_task_status and not
                             instance.analysis_task_status == 'None'):
-                        return
+                        # If the analysis is success
+                        if instance.analysis_task_status == 'SUCCESS':
+                            # If the report task is not None
+                            if (instance.report_task_status and not
+                                    instance.report_task_status == 'None'):
+                                return
+                            # If the report task is None, need to generate
+                            else:
+                                LOGGER.info('Sending task generate report.')
+                                result = generate_ash_report.delay(instance)
+                                instance.report_task_id = result.id
+                                instance.report_task_status = 'PENDING'
+                                instance.save()
+                        else:
+                            return
                     # No task status, or status == None, need to run the task.
                     else:
+                        LOGGER.info('Sending task run analysis.')
                         result = run_ash_analysis.delay(instance)
                         instance.analysis_task_id = result.id
                         instance.analysis_task_status = 'PENDING'
