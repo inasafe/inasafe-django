a6717e6bfc1b0d6f6c5c3679c2f7f95f8e19ca2f
Ismail Sunni <imajimatika@gmail.com>
2018-02-24 16:18:57 +0700

    Integrate flood realtime with inasafe-headless.

--- django_project/realtime/tasks/test/test_realtime_tasks.py
+++ django_project/realtime/tasks/test/test_realtime_tasks.py
@@ -1,11 +1,15 @@
 # coding=utf-8
 """Docstring here."""
+import os
 import unittest
+
 from django import test
 
 from realtime.tasks.realtime.flood import process_flood
 from realtime.tasks.flood import create_flood_report
 from realtime.tasks.realtime.celery_app import app as realtime_app
+
+flood_data_path = '/home/realtime/hazard-drop/flood_data.json'
 
 
 @unittest.skipUnless(
@@ -13,12 +17,20 @@ from realtime.tasks.realtime.celery_app import app as realtime_app
 class TestRealtimeCeleryTask(test.SimpleTestCase):
     """Unit test for Realtime Celery tasks."""
 
-    def test_process_flood(self):
-        """Test process flood."""
-        async_result = process_flood.delay()
-        result = async_result.get()
-        self.assertTrue(result['success'])
-
     def test_create_flood_report(self):
         """Test Create Flood report task"""
         create_flood_report()
+
+    @unittest.skipUnless(
+        os.path.exists(flood_data_path),
+        'Skip because this test need flood data in %s' % flood_data_path)
+    def test_process_flood_manually(self):
+        """Test process flood with existing flood json."""
+        process_flood.delay(
+            # flood_id='2018022205-6-rw',
+            flood_id='',
+            data_source='hazard_file',
+            data_source_args={
+                'filename': flood_data_path
+            }
+        )
