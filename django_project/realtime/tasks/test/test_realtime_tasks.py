b733140c71a5745964b6f7a194b024e6ad564f5b
Ismail Sunni <imajimatika@gmail.com>
2018-02-26 10:23:39 +0700

    Put test data in repo, fix code based on Rizky's comments.

--- django_project/realtime/tasks/test/test_realtime_tasks.py
+++ django_project/realtime/tasks/test/test_realtime_tasks.py
@@ -9,7 +9,9 @@ from realtime.tasks.realtime.flood import process_flood
 from realtime.tasks.flood import create_flood_report
 from realtime.tasks.realtime.celery_app import app as realtime_app
 
-flood_data_path = '/home/realtime/hazard-drop/flood_data.json'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+flood_layer_uri = os.path.join(
+    dir_path, 'data', 'input_layers', 'flood_data.json')
 
 
 @unittest.skipUnless(
@@ -21,15 +23,12 @@ class TestRealtimeCeleryTask(test.SimpleTestCase):
         """Test Create Flood report task"""
         create_flood_report()
 
-    @unittest.skipUnless(
-        os.path.exists(flood_data_path),
-        'Skip because this test need flood data in %s' % flood_data_path)
     def test_process_flood_manually(self):
         """Test process flood with existing flood json."""
         process_flood.delay(
-            flood_id='2018022411-6-rw',
+            flood_id='2018022511-6-rw',
             data_source='hazard_file',
             data_source_args={
-                'filename': flood_data_path
+                'filename': flood_layer_uri
             }
         )
