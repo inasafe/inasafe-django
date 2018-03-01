749a80e36450b09a2ccde7ecff93a434e221b79f
Ismail Sunni <imajimatika@gmail.com>
2018-03-01 07:28:13 +0700

    Update command to push flood data for demo.

--- django_project/realtime/management/commands/loadfloodtestdata.py
+++ django_project/realtime/management/commands/loadfloodtestdata.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+import os
+import shutil
+from tempfile import mkdtemp
+
 from django.core.management.base import BaseCommand
 
 from realtime.tasks.test.test_realtime_tasks import flood_layer_uri
@@ -12,6 +16,16 @@ class Command(BaseCommand):
     help = 'Script to load flood test data for demo purpose only.'
 
     def handle(self, *args, **options):
+        # Copy file to hazard drop directory
+        REALTIME_HAZARD_DROP = os.environ.get(
+            'REALTIME_HAZARD_DROP',
+            '/home/realtime/hazard-drop/')
+        hazard_drop_path = mkdtemp(dir=REALTIME_HAZARD_DROP)
+        hazard_drop_path = os.path.join(
+            hazard_drop_path, os.path.basename(flood_layer_uri))
+        print 'Copy flood data to %s' % hazard_drop_path
+        shutil.copy(flood_layer_uri, hazard_drop_path)
+
         flood_id = '2018022511-6-rw'
         print 'Send flood data to InaSAFE Django with flood id = %s' % flood_id
         process_flood.delay(
