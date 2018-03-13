ba4f89fad26c9237f341620b27a20b8f3c34f2eb
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-03-13 14:18:25 +0700

    More refactor cleanup on celery tasks

--- django_project/realtime/signals/flood.py
+++ django_project/realtime/signals/flood.py
@@ -5,8 +5,8 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
 from realtime.app_settings import LOGGER_NAME
-from realtime.models.flood import Flood, FloodEventBoundary, BoundaryAlias
-from realtime.tasks.flood import generate_event_report, process_hazard_layer
+from realtime.models.flood import Flood
+from realtime.tasks.flood import generate_event_report
 
 __author__ = 'Rizky Maulana Nugraha <lana.pcfre@gmail.com>'
 __date__ = '12/4/15'
@@ -34,11 +34,3 @@ def flood_post_save(
             generate_event_report.delay(instance)
     except Exception as e:
         LOGGER.exception(e)
-
-    # FIXME(IS): I use it to skip it in travis.
-    if BoundaryAlias.objects.all():
-        # Create FloodEventBoundary (to be shown in the map)
-        if not FloodEventBoundary.objects.filter(flood=instance):
-            process_hazard_layer.delay(instance)
-    else:
-        LOGGER.warning('No Boundary alias, not running process_hazard_layer.')
