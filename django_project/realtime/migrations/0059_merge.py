89b5dd23febab86b43a202361efb8d67c0217bab
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-05-02 05:34:21 +0700

    Create migration file merges

---
+++ django_project/realtime/migrations/0059_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('realtime', '0058_auto_20180428_2030'),
+        ('realtime', '0053_volcano_timezone'),
+    ]
+
+    operations = [
+    ]
