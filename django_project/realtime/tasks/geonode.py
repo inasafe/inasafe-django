05d80a2ef54bc3aa434b58c687bce1b370d67b05
Ismail Sunni <imajimatika@gmail.com>
2018-06-07 20:42:23 +0700

    Add hacky way to make the celery task work for geonode push.

--- django_project/realtime/tasks/geonode.py
+++ django_project/realtime/tasks/geonode.py
@@ -16,32 +16,43 @@ from realtime.tasks.headless.inasafe_wrapper import push_to_geonode
 LOGGER = logging.getLogger(LOGGER_NAME)
 GEONODE_PUSH_SUCCESS = 0
 
-hazard_class_mapping = {
-    Ash.__name__: Ash,
-    Flood.__name__: Flood,
-    Earthquake.__name__: Earthquake
-}
-
 
 @app.task(queue='inasafe-django')
 def handle_push_to_geonode(push_result, hazard_class_name, hazard_event_id):
     """Handle geonode push result."""
+    hazard_class_mapping = {
+        Ash.__name__: Ash,
+        Flood.__name__: Flood,
+        Earthquake.__name__: Earthquake
+    }
+
+    # Hacky thing to get proper class
+    if hazard_class_name not in hazard_class_mapping.keys():
+        for key in hazard_class_mapping.keys():
+            if key.lower() in hazard_class_name.lower():
+                hazard_class_name = key
+                break
     hazard_class = hazard_class_mapping.get(hazard_class_name)
-    hazard = hazard_class.objects.get(id=hazard_event_id)
     task_state = 'FAILURE'
     if not push_result:
         task_state = 'FAILURE'
     elif push_result['status'] == GEONODE_PUSH_SUCCESS:
         task_state = 'SUCCESS'
-    hazard.push_task_status = task_state
-    hazard.push_task_result = push_result
-    hazard.save()
+
+    # Use update so it doesn't trigger save signal
+    hazard_class.objects.filter(id=hazard_event_id).update(
+        push_task_status=task_state,
+        push_task_result=push_result
+    )
 
 
 @app.task(queue='inasafe-django')
 def push_hazard_to_geonode(hazard_event):
     """Upload layer to geonode and update the status of hazard."""
     LOGGER.info('Push layer to geonode.')
+    # Skip if it's already running
+    if hazard_event.push_task_status:
+        return
     hazard_layer_uri = hazard_event.hazard_path
     hazard_event_id = hazard_event.id
     hazard_class = hazard_event.__class__
@@ -57,7 +68,7 @@ def push_hazard_to_geonode(hazard_event):
         handle_push_to_geonode.s(
             hazard_class_name,
             hazard_event_id
-        )
+        ).set(queue=handle_push_to_geonode.queue)
     )
 
     @app.task
