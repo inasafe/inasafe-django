8b1bb9356b24ebec149f2b27aa30251a84068c57
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-01-09 16:32:53 +0700

    Handle duplicate boundaries
    Boundary additions needs to be guarded with atomic transactions.
    Add command helper to validate boundary table

---
+++ django_project/realtime/management/commands/validateboundarytable.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+
+from realtime.models import Boundary
+
+__author__ = 'Rizky Maulana Nugraha "lucernae" <lana.pcfre@gmail.com>'
+__date__ = '07/09/15'
+
+
+class Command(BaseCommand):
+    """Command to validate boundary tables
+
+    """
+    help = 'Command to validate boundary tables'
+
+    def handle(self, *args, **options):
+        # Find duplicate boundary and delete it
+        aggregate_boundaries = Boundary.objects.values(
+            'upstream_id',
+            'name',
+            'parent').annotate(boundary_count=Count('name'))
+        duplicate_boundaries = aggregate_boundaries.filter(
+            boundary_count__gt=1)
+
+        print('Total duplicate boundaries: {}'.format(
+            duplicate_boundaries.count()))
+
+        for b in duplicate_boundaries:
+            boundary = Boundary.objects.filter(upstream_id=b['upstream_id'])
+            print('Boundary upstream id: {}'.format(b['upstream_id']))
+            boundary.delete()
